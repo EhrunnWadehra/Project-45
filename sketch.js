be96809782d981c1970ae0cb21a0c482763bcb05
EhrunnWadehra <74880658+EhrunnWadehra@users.noreply.github.com>
2021-02-02 15:58:16 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   Toucan.addImage(ToucanImage);
   Toucan.scale = 0.5
   
-  cerealGroup = new Group();
+  // cerealGroup = new Group();
 
   
 }
@@ -32,12 +32,17 @@ function draw() {
  }
 
   spawnCereal();
- if(cereal) {
+ /* if(cereal) {
  if(cerealGroup.isTouching(Toucan)) {
    cerealGroup.remove(cereal);
    cereal.remove()
    console.log(cereal)
  }
+ } */
+
+ if(cereal && isTouching(cereal, Toucan)) {
+  cereal.remove()
+
  }
   drawSprites();
 }
@@ -50,9 +55,15 @@ function spawnCereal(){
   cereal.velocityY = -3;
   cereal.scale = 0.25;
   cereal.x = Math.round(random(50, windowWidth))
+
   cereal.lifetime = 1200;
   
-  cerealGroup.add(cereal);
+  //cerealGroup.add(cereal);
+
+  /* if (isTouching(Toucan, cereal)) {
+    cereal.remove()
+   } */
+
   }
 }
 
@@ -60,3 +71,28 @@ function spawnCereal(){
   cereal.remove();
 } */
 
+function isTouching(object1,object2){
+  if ((object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2)
+    ||(object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2)) {
+      console.log("yes")
+      console.log("Toucan x position: ", object2.x)
+      console.log("cereal x position: ", object1.x)
+      console.log("Toucan y position: ", object2.y)
+      console.log("cereal y position: ", object1.y)
+      console.log("Toucan height: ", object2.height)
+      console.log("cereal height: ", object1.height)
+      console.log("Toucan width: ", object2.width)
+      console.log("cereal width: ", object1.width)
+
+
+    return true;
+  }
+  else {
+    console.log("no")
+
+    return false;
+
+  } 
+}
